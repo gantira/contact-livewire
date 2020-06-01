10b42986154b56ed9a621d5030369ba620d98e63
Angga Gantira <angga.gantira@gmail.com>
2020-06-01 21:03:26 +0700

    Laravel Livewire - 03 Menampilkan Data Contact dalam Table

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -1,7 +1,25 @@
 <div>
-    <ul>
-        @foreach ($contacts as $row)
-        <li>{{ $row->name }}</li>
-        @endforeach
-    </ul>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($contacts as $key => $row)
+            <tr>
+                <th scope="row">{{ $key+1 }}</th>
+                <td>{{ $row->name }}</td>
+                <td>{{ $row->phone }}</td>
+                <td>
+                    <button class="btn btn-sm btn-info text-white">Edit</button>
+                    <button class="btn btn-sm btn-danger text-white">Delete</button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
